96bada34ba7f8382f901e29aaeacb00dbc9aee80
RijalArul <fakhrulmrijal7599@gmail.com>
2021-11-15 20:46:40 +0700

    Updated render reusable component

--- src/components/CardTodos.js
+++ src/components/CardTodos.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import * as moment from 'moment'
 
-function CardTodos () {
+function CardTodos ({ todo }) {
   return (
     <>
       <div class='card mt-3 mb-3'>
         <div class='card-body-todos'>
           <h5>
-            This is some text within a card body, 14 November 2021 08:20:17{' '}
+            {`${todo.title}, ${moment(todo.due_on).format(
+              `MMMM Do YYYY, h:mm:ss a`
+            )}`}{' '}
             <select className='select-active'>
               <option>1</option>
               <option>1</option>
