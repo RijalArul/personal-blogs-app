949c60d6048b8e8ca3a4310b02383b10d47079d7
RijalArul <fakhrulmrijal7599@gmail.com>
2021-11-16 12:25:14 +0700

    Updated handle delete click

--- src/components/CardTodos.js
+++ src/components/CardTodos.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import * as moment from 'moment'
 import { useSelector } from 'react-redux'
 
 function CardTodos ({ todo }) {
-  console.log(todo)
+  const [statusTodo, setStatusTodo] = useState()
   const { status } = useSelector(state => state.todoState)
+  useEffect(() => {
+    setStatusTodo({
+      statusTodo: todo.status
+    })
+  }, [])
+
+  function handleChange (e) {
+    setStatusTodo({
+      ...statusTodo,
+      statusTodo: e.target.value
+    })
+  }
+
+  function deleteTodo (id) {
+    console.log(id)
+  }
   return (
     <>
       <div class='card mt-3 mb-3'>
@@ -13,21 +29,21 @@ function CardTodos ({ todo }) {
             {`${todo.title}, ${moment(todo.due_on).format(
               `MMMM Do YYYY, h:mm:ss a`
             )}`}{' '}
-            <select className='select-active' value={todo.status}>
+            <select
+              className='select-active'
+              value={statusTodo}
+              onChange={e => handleChange(e)}
+            >
               {status &&
                 status.map(status => {
                   return (
                     <>
-                      <option value={status.value}>
-                        {todo.status === status.value
-                          ? todo.status
-                          : status.value}
-                      </option>
+                      <option value={status.value}>{status.value}</option>
                     </>
                   )
                 })}
             </select>
-            <button>Delete</button>
+            <button onClick={id => deleteTodo(todo.id)}>Delete</button>
           </h5>
         </div>
       </div>
